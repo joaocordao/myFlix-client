8626cd8fc3040a2f181158924e76f824bd937418
joaocordao <jp.cordao@gmail.com>
2024-06-04 11:01:02 +0200

    Create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,11 @@
+export const MainView0 = () => {
+    return (
+        <div>
+            <div>Eloquent JavaScript</div>
+            <div>Mastering JavaScript Functional Programming</div>
+            <div>JavaScript: The Good Parts</div>
+            <div>JavaScript: The Definitive Guide</div>
+            <div>The Road to React</div>
+        </div>
+    );
+}
